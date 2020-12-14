371a710cbb2078085c1949903818888ceb478313
ZatsepaTanya <Tanyamath@i.ua>
2020-12-14 22:28:17 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,37 +1,48 @@
+const findBestEmployee = function(employees) {
+    const values = Object.values(employees);
+       console.log(values);
+    let bestCount = values[0];
+    for(let i=1; i<=values.length; i+=1){
+        if(bestCount<values[i]){
+        bestCount=values[i];
+        }
+       
+    }
+    console.log(bestCount);
+    // console.log(this.Object);
+    
+    // for(const key in employees){
+    //     console.log(key);
+    // }
+    
+};
 // Напиши функцию findBestEmployee(employees), 
 // которая принимает объект сотрудников и возвращает имя самого продуктивного 
 // (который выполнил больше всех задач). Сотрудники и кол-во выполненых задач 
 // содержатся как свойства объекта в формате "имя":"кол-во задач".
 
-// const findBestEmployee = function(employees) {
-//   // твой код
-// };
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
 
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(
-//   findBestEmployee({
-//     ann: 29,
-//     david: 35,
-//     helen: 1,
-//     lorence: 99,
-//   }),
-// ); // lorence
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
 
-// console.log(
-//   findBestEmployee({
-//     poly: 12,
-//     mango: 17,
-//     ajax: 4,
-//   }),
-// ); // mango
-
-// console.log(
-//   findBestEmployee({
-//     lux: 147,
-//     david: 21,
-//     kiwi: 19,
-//     chelsy: 38,
-//   }),
-// ); // lux
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
--- js/task-5.js
+++ js/task-5.js
@@ -1,15 +1,17 @@
-// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства. Возвращает массив значений определенного свойства prop из каждого объекта в массиве.
+// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов
+//  и имя свойства. Возвращает массив значений определенного свойства prop
+//   из каждого объекта в массиве.
 
-// const products = [
-//   { name: 'Радар', price: 1300, quantity: 4 },
-//   { name: 'Сканер', price: 2700, quantity: 3 },
-//   { name: 'Дроид', price: 400, quantity: 7 },
-//   { name: 'Захват', price: 1200, quantity: 2 },
-// ];
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
 
-// const getAllPropValues = function(arr, prop) {
-//   // твой код
-// };
+const getAllPropValues = function(arr, prop) {
+  
+};
 
 // /*
 //  * Вызовы функции для проверки работоспособности твоей реализации.
