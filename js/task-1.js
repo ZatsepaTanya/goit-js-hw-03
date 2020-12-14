33362269da575bd156f095090f942c0c4f5a9ba8
ZatsepaTanya <Tanyamath@i.ua>
2020-12-15 00:00:13 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -4,9 +4,17 @@
 // заменяет значение hobby на 'skydiving'
 // заменяет значение premium на false
 // выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
-// const user = {
-//   name: 'Mango',
-//   age: 20,
-//   hobby: 'html',
-//   premium: true,
-// };
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+const keys = Object.keys(user);
+for(const key of keys){
+    console.log(key,':',user[key]);
+}
